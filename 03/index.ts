23633772c455b3d8750c11fa5adfdc094e428363
Thomas Beirne <thomas.beirne@gmail.com>
2021-12-03 13:53:52 -0800

    Day 3, part 2

--- 03/index.ts
+++ 03/index.ts
@@ -8,13 +8,10 @@ export function part1(diagnostic: string[]): number {
 			if (bit === '1') numOnesByPlace[i] += 1
 		})
 	})
-
-	const gamma = parseInt(
-		numOnesByPlace
-			.map((c) => (c >= diagnostic.length / 2 ? '1' : '0'))
-			.join(''),
-		2
-	)
+	const gammaStr = numOnesByPlace
+		.map((c) => (c >= diagnostic.length / 2 ? '1' : '0'))
+		.join('')
+	const gamma = parseInt(gammaStr, 2)
 
 	// Bitwise NOT gamma to get epsilon, but mask it to only the digits we care about.
 	const epsilon = ~gamma & (2 ** numDigits - 1)
@@ -22,6 +19,36 @@ export function part1(diagnostic: string[]): number {
 	return gamma * epsilon
 }
 
+function getRating(
+	diagnostic: string[],
+	type: 'OXYGEN_GENERATOR' | 'CO2_SCRUBBER'
+): number {
+	let candidates = [...diagnostic]
+	const numDigits = diagnostic[0].length
+
+	for (let i = 0; i < numDigits; i++) {
+		const numOnes = candidates.reduce((n, c) => (c[i] === '1' ? n + 1 : n), 0)
+		const mostCommonDigit = numOnes >= candidates.length / 2 ? '1' : '0'
+
+		candidates = candidates.filter(
+			type === 'OXYGEN_GENERATOR'
+				? (c) => c[i] === mostCommonDigit
+				: (c) => c[i] !== mostCommonDigit
+		)
+
+		if (candidates.length === 1) return parseInt(candidates[0], 2)
+	}
+	throw new Error('Uh oh, failed to filter to a single number')
+}
+
+export function part2(diagnostic: string[]): number {
+	return (
+		getRating(diagnostic, 'OXYGEN_GENERATOR') *
+		getRating(diagnostic, 'CO2_SCRUBBER')
+	)
+}
+
 run(() => {
 	console.log('Part 1:', part1(getInputLines(__dirname)))
+	console.log('Part 2:', part2(getInputLines(__dirname)))
 })
